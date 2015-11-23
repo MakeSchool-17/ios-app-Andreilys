64ba57f1f7b1f279e0fb30ad915fb0e0ab33fc00
Andrei Lyskov <andreilyskov@gmail.com>
2015-11-22 21:22:43 -0800

    updating article

---
+++ Project Reciprocity/Project Reciprocity/loginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  loginViewController.swift
+//  Project Reciprocity
+//
+//  Created by Andrei Lyskov on 11/20/15.
+//  Copyright © 2015 Andrei Lyskov. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
